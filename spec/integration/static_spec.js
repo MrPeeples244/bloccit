78d69eafbf5aa46534c0d1942bd3571f1a13debb
jordan.crutcher <jordan.crutcher@gmail.com>
2018-10-06 19:40:56 -0500

    express intro assignment complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
@@ -9,6 +10,16 @@ describe("routes : static", () => {
         expect(res.statusCode).toBe(200);
         done();
       });
+    });
+  });
+});
+
+describe("GET /marco", () => {
+  it("should return status code 200 and have 'polo' in the body", (done) => {
+    request.get(marco, (err, res, body) => {
+      expect(res.statusCode).toBe(200);
+      expect(body).toBe("polo");
+      done();
     });
   });
 });
