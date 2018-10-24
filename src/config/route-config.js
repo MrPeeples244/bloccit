66a45d44a59d15ee743bfa64c267470aee4eeb6e
jordan.crutcher <jordan.crutcher@gmail.com>
2018-10-23 22:38:06 -0500

    post-resource2-checkpoint-complete

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  init(app){
+  init(app) {
     const staticRoutes = require("../routes/static");
     const topicRoutes = require("../routes/topics");
+    const postRoutes = require("../routes/posts");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
-  },
-}
+    app.use(postRoutes);
+  }
+};
