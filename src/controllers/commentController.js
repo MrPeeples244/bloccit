67c6572e1539d8a95a0383fe33e84eb61f9c64e9
jordan.crutcher <jordan.crutcher@gmail.com>
2018-11-26 07:44:29 -0600

    WIP2

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,20 +3,16 @@ const Authorizer = require("../policies/comment.js");
 
 module.exports = {
   create(req, res, next) {
-    // #2
     const authorized = new Authorizer(req.user).create();
 
     if (authorized) {
-      // #3
       let newComment = {
         body: req.body.body,
         userId: req.user.id,
         postId: req.params.postId
       };
 
-      // #4
       commentQueries.createComment(newComment, (err, comment) => {
-        // #5
         if (err) {
           req.flash("error", err);
         }
@@ -24,11 +20,10 @@ module.exports = {
       });
     } else {
       req.flash("notice", "You must be signed in to do that.");
-      req.redirect("/users/sign_in");
+      res.redirect("/users/sign_in");
     }
   },
 
-  // #6
   destroy(req, res, next) {
     commentQueries.deleteComment(req, (err, comment) => {
       if (err) {
